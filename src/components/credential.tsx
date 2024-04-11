f8556f2c58d5438b5d03d8ec990b42f714327928
eolucaslopes <eolucaslopes@gmail.com>
2024-04-10 23:48:27 -0300

    feat: adiciona componente credencial

--- src/components/credential.tsx
+++ src/components/credential.tsx
@@ -1,7 +1,36 @@
-import { View } from "react-native";
+import { View, Image, ImageBackground, Text } from "react-native";
 
 export function Credential() {
   return (
-    <View></View>
+    <View className="w-full self-stretch items-center">
+      <Image 
+        source={require("@/assets/ticket/band.png")}
+        className="w-24 h-52 z-10" 
+      />
+
+      <View className="bg-black/20 self-stretch items-center pb-6 border border-white/10 
+      mx-3 rounded-2xl -mt-5">
+        <ImageBackground 
+          source={require("@/assets/ticket/header.png")} 
+          className="px-6 py-8 h-40 items-center self-stretch border-b border-white/10 overflow-hidden"
+        >
+          <View className="w-full flex-row items-center justify-between">
+            <Text className="text-zinc-50 text-sm font-bold">Unite summit</Text>
+            <Text className="text-zinc-50 text-sm font-bold">#123</Text>
+          </View>  
+
+          <View className="w-40 h-40 bg-black rounded-full" />
+        </ImageBackground>
+
+        <Image 
+          source={{ uri: "https://avatars.githubusercontent.com/u/164359615?v=4"}} 
+          className="w-36 h-36 rounded-full -mt-24"
+        />
+
+        <Text className="font-bold text-2xl text-zinc-50 mt-4">
+          Lucas Lopes
+        </Text>
+      </View>
+    </View>
   )
 }
