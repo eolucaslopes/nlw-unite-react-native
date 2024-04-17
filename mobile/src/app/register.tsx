bf793f69a54231c8a5175ece6a19509d785321b3
eolucaslopes <eolucaslopes@gmail.com>
2024-04-17 00:02:21 -0300

    feat: remove servidor, conservando apenas o frontend.

--- mobile/src/app/register.tsx
+++ mobile/src/app/register.tsx
@@ -2,21 +2,50 @@ import { useState } from 'react';
 import { View, StatusBar, Image, Platform, Alert } from 'react-native';
 import { FontAwesome6, MaterialIcons } from '@expo/vector-icons';
 import { Link, router } from 'expo-router';
+import axios from 'axios';
+
+import { colors } from '@/styles/colors';
+
+import { api } from '@/server/api';
 
 import { Input } from '@/components/input';
 import { Button } from '@/components/button';
-import { colors } from '@/styles/colors';
+
+const EVENT_ID = '9e9bd979-9d10-4915-b339-3786b1634f33'
 
 export default function Register() {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
 
-  function handelRegister() {
-    if (!name.trim() || !email.trim()) {
-      return Alert.alert('Inscrição', 'Preencha todos os campos!')
+  async function handelRegister() {
+    try {
+      if (!name.trim() || !email.trim()) {
+        return Alert.alert('Inscrição', 'Preencha todos os campos!')
+      }
+
+      setIsLoading(true)
+
+      const resgisterResponse = await api.post(`/events/${EVENT_ID}/attendees`, { name, email })
+
+      if (resgisterResponse.data.attendeeID) {
+        Alert.alert('Inscrição', 'Inscrição realizada com sucesso!', [
+          { text: 'OK', onPress: () => router.push('/ticket') }
+        ])
+      }
+      
+    } catch (error) {
+        console.log(error)
+
+        if(axios.isAxiosError(error)) {
+          if(String(error.response?.data.message).includes('already registered'))
+            return Alert.alert('Inscrição', 'Este e-mail já está cadastrado!')
+        }
+
+        Alert.alert('Inscrição:', 'Não foi possível fazer a inscrição')
+    } finally {
+      setIsLoading(false)
     }
-
-    router.push('/ticket')
   }
 
   return (
@@ -47,7 +76,7 @@ export default function Register() {
           <Input.Field placeholder="E-mail" keyboardType="email-address" onChangeText={setEmail} />
         </Input>
 
-        <Button title="Realizar inscrição" onPress={handelRegister} />
+        <Button title="Realizar inscrição" onPress={handelRegister} isLoading={isLoading} />
 
         <Link href="/" className="text-gray-100 text-base font-bold
         text-center mt-8">
